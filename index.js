bc4b0d14ef00205ad216539b4957ffc33d4db04e
HOSAIN MAHMUD <hosainmahmud191@gmail.com>
2023-11-22 18:22:16 +0600

    navber added in each file

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // import some package
-const path = require ('path')
+const path = require('path')
 const express = require('express');
 const app = express() //app is my backend site
 
 app.get('/', function (req, res) {
     //res.sendFile('D:/Programming language/JS/views/Home.html')
     //res.sendFile(__dirname + "/views/Home.html")
-    res.sendFile(path.join(__dirname, "views","Home.html"))
+    res.sendFile(path.join(__dirname, "views", "Home.html"))
 })
 
 app.get('/About', function (req, res) {
-    res.sendFile(path.join(__dirname, "views","About.html"))
+    res.sendFile(path.join(__dirname, "views", "About.html"))
 })
 
 app.get('/Contact', function (req, res) {
-    res.sendFile(path.join(__dirname, "views","Contact.html"))
+    res.sendFile(path.join(__dirname, "views", "Contact.html"))
 })
 
 app.get('*', function (req, res) {  //'*' use for unused page
-    res.send('<h1>Page not Found!!</h1>')
+    res.sendFile(path.join(__dirname, "views", "404.html"))
 })
 //Listening a port 
 app.listen(3000)
